5086866010f536513abf8449dc5b36025ef3f62d
tatid <tatidunina@gmail.com>
2025-11-18 00:25:30 +0300

    add HTML files, add styles

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,24 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
 
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def form():
+    return render_template('form.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        color = request.form.get('color')
+        profession = request.form.get('profession')
+        hobbies = request.form.getlist('hobbies')
+        age = request.form.get('age')
 
-
-@app.route('/contacts')
-def contacts():
-    return render_template('contacts.html')
+        return render_template('result.html',
+                               name=name, email=email, color=color,
+                               profession=profession, hobbies=hobbies,
+                               age=age)
+    else:
+        return redirect(url_for('form'))
